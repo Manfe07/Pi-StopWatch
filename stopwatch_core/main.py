827f8422111c060a1b16259a8c5266d286898b1b
Manuel Fehren <Manuel.Fehren@Gmail.com>
2020-09-23 17:38:17 +0200

    added stopwatch_core

---
+++ stopwatch_core/main.py
@@ -0,0 +1,17 @@
+import time
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+
+client.connect("localhost", 1883, 60)
+
+client.loop_start()
+
+while(1):
+    time.sleep(2)
+    client.publish("test/temperature", "test")
+
