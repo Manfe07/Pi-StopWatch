74e5643db6c9e4d55b2e5da7f229b4f9095807a9
Manuel Fehren <Manuel.Fehren@Gmail.com>
2020-09-24 20:37:35 +0200

    added button_status on interface

--- flask_backend/main.js
+++ flask_backend/main.js
@@ -1,5 +1,1 @@
 // JavaScript Document
-function refresh_duration(){
-	document.getElementById("duration").innerHTML = "Paragraph changed!";
-	return 0
-}
--- flask_backend/main.py
+++ flask_backend/main.py
@@ -1,4 +1,6 @@
 #import commands
+import json, random
+
 from flask import Flask, render_template
 #import paho.mqtt.subscribe as subscribe
 
@@ -13,9 +15,20 @@ def index():
 @app.route("/data")
 def data():
     stuff = {
-        "time":00,
-        "Name":"Manuel"
+        "Button_1": False,
+        "Button_2": False,
+        "Button_3": False,
+        "Name": "Manuel"
     }
+    stuff["Button_1"] = bool(random.getrandbits(1))
+    stuff["Button_2"] = bool(random.getrandbits(1))
+    stuff["Button_3"] = bool(random.getrandbits(1))
+    return json.dumps(stuff)
 
+@app.route("/get_ip")
+def get_ip():
+    data = {"ip": "192.168.0.1"}
+    data["ip"] = str(random.randrange(0,255,1)) + "." + str(random.randrange(0,255,1)) + "." + str(random.randrange(0,255,1)) + "." + str(random.randrange(0,255,1))
+    return json.dumps(data)
 if __name__ == '__main__':
     app.run(port=1337,debug=True)
