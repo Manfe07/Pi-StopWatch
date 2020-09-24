580268f3d50fc6edc666906c6f2660093027f5f3
Manuel Fehren <Manuel.Fehren@Gmail.com>
2020-09-24 10:12:54 +0200

    changed flask rendering

---
+++ flask_backend/main.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+function refresh_duration(){
+	document.getElementById("duration").innerHTML = "Paragraph changed!";
+	return 0
+}
--- flask_backend/main.py
+++ flask_backend/main.py
@@ -1,17 +1,21 @@
-import commands
+#import commands
 from flask import Flask, render_template
-import paho.mqtt.subscribe as subscribe
+#import paho.mqtt.subscribe as subscribe
 
 app = Flask(__name__)
 
-ip = commands.getoutput('hostname -I')
+#ip = commands.getoutput('hostname -I')
 
 @app.route("/")
 def index():
-    start_time = subscribe.simple("stopwatch/start_time", hostname="127.0.0.1")
-    runtime = subscribe.simple("stopwatch/runtime", hostname="127.0.0.1")
-    duration = subscribe.simple("stopwatch/duration", hostname="127.0.0.1")
-    return render_template("index.html", adress=ip, start_time=start_time.payload, runtime=runtime.payload, duration=duration.payload )
+    return render_template("index.html")
+
+@app.route("/data")
+def data():
+    stuff = {
+        "time":00,
+        "Name":"Manuel"
+    }
 
 if __name__ == '__main__':
     app.run(port=1337,debug=True)
