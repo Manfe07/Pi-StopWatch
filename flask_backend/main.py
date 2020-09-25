92e17dd71ced5fbaa6965f32cb6c69ff555c59e4
Manuel Fehren <Manuel.Fehren@Gmail.com>
2020-09-25 21:02:23 +0200

    added MQTT to flask

--- flask_backend/main.py
+++ flask_backend/main.py
@@ -1,10 +1,52 @@
 import commands
 import json, random
 
+from flask_mqtt import Mqtt
 from flask import Flask, render_template
-#import paho.mqtt.subscribe as subscribe
 
 app = Flask(__name__)
+app.config['MQTT_BROKER_URL'] = '127.0.0.1'
+#app.config['MQTT_BROKER_URL'] = '192.168.11.213'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_USERNAME'] = ''
+app.config['MQTT_PASSWORD'] = ''
+app.config['MQTT_REFRESH_TIME'] = 0.1
+mqtt = Mqtt(app)
+mqtt.init_app(app)
+
+ip = "131.123.123.123"
+
+stuff = {
+    "start_time": "",
+    "runtime": "000.00",
+    "running": False,
+    "Button_1": False,
+    "Button_2": False,
+    "Button_3": False,
+}
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    mqtt.subscribe('stopwatch/#')
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    global stuff
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    if(data["topic"] == "stopwatch/running"):
+        if(data["payload"] == "True"):
+            stuff["running"] = True;
+        else:
+            stuff["running"] = False;
+    elif(data["topic"] == "stopwatch/duration"):
+        stuff["runtime"] = data["payload"]
+    elif(data["topic"] == "stopwatch/start_time"):
+        stuff["start_time"] = data["payload"]
+        print("startTime")
+
 
 
 @app.route("/")
@@ -13,12 +55,7 @@ def index():
 
 @app.route("/data")
 def data():
-    stuff = {
-        "Button_1": False,
-        "Button_2": False,
-        "Button_3": False,
-        "Name": "Manuel"
-    }
+    global stuff
     stuff["Button_1"] = bool(random.getrandbits(1))
     stuff["Button_2"] = bool(random.getrandbits(1))
     stuff["Button_3"] = bool(random.getrandbits(1))
