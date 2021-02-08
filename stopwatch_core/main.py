0286ab01df9586971ca9bdb98cd516282a5d4339
Manuel Fehren <Manuel.Fehren@Gmail.com>
2021-02-08 15:41:32 +0100

    Update main.py

--- stopwatch_core/main.py
+++ stopwatch_core/main.py
@@ -3,6 +3,11 @@ import paho.mqtt.client as mqtt
 import paho.mqtt.subscribe as subscribe
 import stopwatch
 
+with open("config.json") as json_data_file:
+    config = json.load(json_data_file)
+
+mqttHost = config["mqtt"]["host"]
+mqttPort = config["mqtt"]["port"]
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     client.subscribe("stopwatch/webInput/#")
@@ -20,7 +25,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
+client.connect(mqttHost, mqttPort, 60)
 
 client.loop_start()
 
