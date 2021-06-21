3e0ee7d15a5374128856ed40ec70254ee84df059
Manuel Fehren <Manuel.Fehren@Gmail.com>
2021-06-21 18:25:10 +0200

    Squashed commit of the following:
    commit 7b70632887298e99cba44a31fb8b23b92fec3597
    Author: Manuel Fehren <Manuel.Fehren@Gmail.com>
    Date:   Mon Jun 21 17:08:36 2021 +0200
    
        moved usv tools to Stopwatch core
    
        moved UPS function to core
    
        updated ups function
    
        Update requirements.txt
    
        Update main.py
    
        fixed UPS plugin implementation
    
        fixed plugin problem
    
        fixed typo
    
    commit 7db1cbfd3db8219c07318157f02615a2001f10dc
    Author: Manuel Fehren <Manuel.Fehren@Gmail.com>
    Date:   Mon Jun 21 17:00:08 2021 +0200
    
        added if __name__ == '__main__'
    
    commit 4a32114054043ecbf2402eb2817fd5d3ba27d2d0
    Author: Manuel Fehren <Manuel.Fehren@Gmail.com>
    Date:   Mon Jun 21 16:49:53 2021 +0200
    
        Update main.py
    
    commit 42d1e6bf082d0793f7034a17ed2c24c6a08910b1
    Author: Manuel Fehren <Manuel.Fehren@Gmail.com>
    Date:   Tue Jun 15 15:17:05 2021 +0200
    
        implemented INA219 UPS for Pi
    
    commit 8b17f43011150432037bfecdcb0235643276eb1f
    Author: Manuel Fehren <Manuel.Fehren@Gmail.com>
    Date:   Mon Feb 8 15:50:11 2021 +0100
    
        Update main.py
    
    commit b1a8ca2bc1d05645db5410d916700d3316b6d224
    Author: Manuel Fehren <Manuel.Fehren@Gmail.com>
    Date:   Mon Feb 8 15:50:08 2021 +0100
    
        Update config.json
    
    commit 58da0675ea5c6fa915b00cb81d040d968f4e8d87
    Author: Manuel Fehren <Manuel.Fehren@Gmail.com>
    Date:   Mon Feb 8 15:45:49 2021 +0100
    
        moved config.json to top folder
    
    commit 0286ab01df9586971ca9bdb98cd516282a5d4339
    Author: Manuel Fehren <Manuel.Fehren@Gmail.com>
    Date:   Mon Feb 8 15:41:32 2021 +0100
    
        Update main.py
    
    commit f360ad4de4561abdbb3313693f609d458d88e499
    Author: Manuel Fehren <Manuel.Fehren@Gmail.com>
    Date:   Mon Feb 8 15:41:27 2021 +0100
    
        Update config.json
    
    commit 14c13ad3b608858ce4c5c859b628219935d041dc
    Author: Manuel Fehren <Manuel.Fehren@Gmail.com>
    Date:   Mon Feb 8 15:37:30 2021 +0100
    
        Update stopwatch.py
    
    commit 6305c87b37da06fac2f2da5562676747b8fe602c
    Author: Manuel Fehren <Manuel.Fehren@Gmail.com>
    Date:   Mon Feb 8 15:37:22 2021 +0100
    
        Create config.json

--- flask_backend/main.py
+++ flask_backend/main.py
@@ -1,15 +1,21 @@
 import os
 #import commands
-import json, random, datetime
-import x750ups as x750
+import json
 
 from flask_mqtt import Mqtt
-from flask import Flask, render_template, send_file, request
+from flask import Flask, render_template, send_file
+
+with open("../config.json") as json_data_file:
+    config = json.load(json_data_file)
+
+
+mqttHost = config["mqtt"]["host"]
+mqttPort = config["mqtt"]["port"]
 
 app = Flask(__name__)
-app.config['MQTT_BROKER_URL'] = '127.0.0.1'
+app.config['MQTT_BROKER_URL'] = mqttHost
 #app.config['MQTT_BROKER_URL'] = '192.168.11.213'
-app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_BROKER_PORT'] = mqttPort
 app.config['MQTT_USERNAME'] = ''
 app.config['MQTT_PASSWORD'] = ''
 app.config['MQTT_REFRESH_TIME'] = 0.1
@@ -31,6 +37,12 @@ stuff = {
     "button_3": False,
 }
 
+ups_data = {
+        "voltage": 0.0,
+        "capacity": 0.0,
+        "current": 0.0
+    }
+
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
     mqtt.subscribe('stopwatch/#')
@@ -38,6 +50,7 @@ def handle_connect(client, userdata, flags, rc):
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
     global stuff
+    global ups_data
     data = dict(
         topic=message.topic,
         payload=message.payload.decode()
@@ -68,6 +81,8 @@ def handle_mqtt_message(client, userdata, message):
             stuff["armed"] = True
         else:
             stuff["armed"] = False
+    elif(data["topic"] == "stopwatch/ups"):
+        ups_data = json.loads(data['payload'])
 
 @app.route("/")
 def index():
@@ -102,14 +117,7 @@ def get_image():
 
 @app.route("/ups")
 def ups():
-    voltage = "{:10.2f}".format(float(x750.getVolage()))
-    capacity = "{:0.0f}".format(float(x750.getCapacity()))
-    data = {
-        "voltage": voltage,
-        "capacity": capacity,
-    }
-    return json.dumps(data)
-
+    return  json.dumps(ups_data)
 
 @app.route("/get_ip")
 def get_ip():
--- stopwatch_core/main.py
+++ stopwatch_core/main.py
@@ -1,7 +1,21 @@
-import time, json
+import json
 import paho.mqtt.client as mqtt
-import paho.mqtt.subscribe as subscribe
 import stopwatch
+import schedule
+import time
+
+with open("../config.json") as json_data_file:
+    config = json.load(json_data_file)
+
+
+if (config["x750"]["enable"] == True):
+    import x750ups as x750
+
+elif (config["INA819"]["enable"] == True):
+    import INA219 as INA219
+
+mqttHost = config["mqtt"]["host"]
+mqttPort = config["mqtt"]["port"]
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -20,7 +34,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
+client.connect(mqttHost, mqttPort, 60)
 
 client.loop_start()
 
@@ -38,41 +52,71 @@ def arm(_state):
         stopwatch.led_Red.set_state(False)
         client.publish("stopwatch/armed", "false", retain=True)
 
-while(1):
-    time.sleep(.01)
-    input = stopwatch.get_input()
-    if(old_input != input):
-        old_input = input
-        print(input)
-        if(input["Button_R"] == True):
-            if(stopwatch.armed):
-                arm(False)
-            else:
-                arm(True)
 
-        client.publish("stopwatch/button", json.dumps(input),retain=True)
-        if ((input["Button_1"] == True) or (input["Button_2"] == True) or (input["Button_3"] == True)) and (stopwatch.armed == True or stopwatch.running == True):
-            if(stopwatch.running == False):
-                stopwatch.start()
-                client.publish("stopwatch/start_time", str(stopwatch.start_time),retain=True)
-                client.publish("stopwatch/time_1", "0.0", retain=True)
-                client.publish("stopwatch/time_2", "0.0", retain=True)
-                client.publish("stopwatch/time_3", "0.0", retain=True)
-                time.sleep(0.5)
-            else:
-                if(input["Button_1"] == True):
-                    stopwatch.lane_1.stop()
-                    client.publish("stopwatch/time_1", str(stopwatch.lane_1.get_duration().total_seconds()), retain=True)
-                if(input["Button_2"] == True):
-                    stopwatch.lane_2.stop()
-                    client.publish("stopwatch/time_2", str(stopwatch.lane_2.get_duration().total_seconds()), retain=True)
-                if(input["Button_3"] == True):
-                    stopwatch.lane_3.stop()
-                    client.publish("stopwatch/time_3", str(stopwatch.lane_3.get_duration().total_seconds()), retain=True)
+def post_UPS():
+    if(config["x750"]["enable"] == True):
+        voltage = "{:10.2f}".format(float(x750.getVolage()))
+        capacity = "{:0.0f}".format(float(x750.getCapacity()))
+        current = 0
+    elif(config["INA819"]["enable"] == True):
+        ups = INA219.INA219(addr=0x42)
+        ups_data = ups.getData()
+        voltage = "{:10.2f}".format(float(ups_data["voltage"]))
+        capacity = "{:0.0f}".format(float(ups_data["percent"]))
+        current = "{:10.2f}".format(float(ups_data["current"]))
+    else:
+        voltage = 0
+        capacity = 0
+        current = 0
 
-                if(stopwatch.check_Finish() == True):
+    data = {
+        "voltage": voltage,
+        "capacity": capacity,
+        "current": current
+    }
+    client.publish("stopwatch/ups", json.dumps(data), retain=True)
+
+
+schedule.every(5).seconds.do(post_UPS)
+
+if __name__ == '__main__':
+    schedule.run_all()
+    while(1):
+        schedule.run_pending()
+        time.sleep(.01)
+        input = stopwatch.get_input()
+        if(old_input != input):
+            old_input = input
+            print(input)
+            if(input["Button_R"] == True):
+                if(stopwatch.armed):
                     arm(False)
+                else:
+                    arm(True)
+
+            client.publish("stopwatch/button", json.dumps(input),retain=True)
+            if ((input["Button_1"] == True) or (input["Button_2"] == True) or (input["Button_3"] == True)) and (stopwatch.armed == True or stopwatch.running == True):
+                if(stopwatch.running == False):
+                    stopwatch.start()
+                    client.publish("stopwatch/start_time", str(stopwatch.start_time),retain=True)
+                    client.publish("stopwatch/time_1", "0.0", retain=True)
+                    client.publish("stopwatch/time_2", "0.0", retain=True)
+                    client.publish("stopwatch/time_3", "0.0", retain=True)
                     time.sleep(0.5)
-            client.publish("stopwatch/running", str(stopwatch.running),retain=True)
-    if(stopwatch.running):
-        client.publish("stopwatch/runtime", stopwatch.get_runtime().total_seconds(),retain=True)
+                else:
+                    if(input["Button_1"] == True):
+                        stopwatch.lane_1.stop()
+                        client.publish("stopwatch/time_1", str(stopwatch.lane_1.get_duration().total_seconds()), retain=True)
+                    if(input["Button_2"] == True):
+                        stopwatch.lane_2.stop()
+                        client.publish("stopwatch/time_2", str(stopwatch.lane_2.get_duration().total_seconds()), retain=True)
+                    if(input["Button_3"] == True):
+                        stopwatch.lane_3.stop()
+                        client.publish("stopwatch/time_3", str(stopwatch.lane_3.get_duration().total_seconds()), retain=True)
+
+                    if(stopwatch.check_Finish() == True):
+                        arm(False)
+                        time.sleep(0.5)
+                client.publish("stopwatch/running", str(stopwatch.running),retain=True)
+        if(stopwatch.running):
+            client.publish("stopwatch/runtime", stopwatch.get_runtime().total_seconds(),retain=True)
